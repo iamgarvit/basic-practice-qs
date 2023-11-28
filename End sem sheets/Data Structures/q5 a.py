81626449673e4acf00be819e699d16cb22089ac0
iamgarvit <87112688+iamgarvit@users.noreply.github.com>
2023-11-28 17:12:50 +0530

    Delete End sem sheets directory

--- End sem sheets/Data Structures/q5 a.py
+++
@@ -1,4 +0,0 @@
-l=list(input().split())
-for i in range(len(l)//2):
-    l[i],l[len(l)-1-i]=l[len(l)-1-i],l[i]
-print(l)
--- End sem sheets/Data Structures/q5 d.py
+++
@@ -1,8 +0,0 @@
-def gp_check(l,r):
-    for i in range(len(l)-1):
-        if(l[i]*r!=l[i+1]):
-            return False
-    return True
-l1=list(map(float,input().split()))
-r1=float(input())
-print(gp_check(l1,r1))
